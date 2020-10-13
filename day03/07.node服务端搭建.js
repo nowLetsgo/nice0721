81b7852600c2d64fd0e3853506261d6c3e880722
nowLetsgo <lipeihua0922@163.com>
2020-10-13 11:51:48 +0800

    day04:上午

--- day03/07.node服务端搭建.js
+++ day03/07.node服务端搭建.js
@@ -6,10 +6,10 @@ const server = http.createServer((request, response) => {
 
 
     //在返回相应之前，需要设置响应头中的数据格式
-    response.setHeader("Content-Type", "text/html;charset=utf-8")
+    response.setHeader("Content-Type", "text/plain;charset=utf-8")
     //返回相应
     response.end("<h1>湖人总冠军</h1>");
-}) 
+})
 
 //启动服务 使用listen方法，三个参数：端口号，主机地址/域名地址，回调函数
 //本地的主机地址：localhost\127.0.0.1 或者是局域网地址 或者是服务器地址
--- day03/08.node服务端返回网页.js
+++ day03/08.node服务端返回网页.js
@@ -2,8 +2,6 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.resolve(__dirname, "./liuwer/index.html");
-const rs = fs.createReadStream(filePath);
 
 /* let str = "";
 rs.on("data", (chunk) => {
@@ -12,20 +10,20 @@ rs.on("data", (chunk) => {
 //创建一个服务
 //createServer的回调函数的形参分别是request和response,代表请求和响应
 const server = http.createServer((request, response) => {
-
-
+    const filePath = path.resolve(__dirname, "./liuwer/index.html");
+    const rs = fs.createReadStream(filePath);
     //在返回相应之前，需要设置响应头中的数据格式
     response.setHeader("Content-Type", "text/html;charset=utf-8")
     //返回相应
-    // rs.pipe(response);
+    rs.pipe(response);
     // response.end(str);
-    response.end("<h1>湖人总冠军</h1>");
+    // response.end("<h1>湖人总冠军</h1>");
 })
 
 //启动服务 使用listen方法，三个参数：端口号，主机地址/域名地址，回调函数
 //本地的主机地址：localhost\127.0.0.1 或者是局域网地址 或者是服务器地址
-const port = 3000;
-const host = "192.168.20.25";
+const port = 3002;
+const host = "127.0.0.1";
 server.listen(port, host, (err) => {
     if (err) {
         console.log("服务器启动失败：" + err);
