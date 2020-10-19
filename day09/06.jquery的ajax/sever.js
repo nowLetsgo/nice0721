ff53963c26b4252a3c1353c31d1a5be942a0e9ee
nowLetsgo <lipeihua0922@163.com>
2020-10-19 15:21:02 +0800

    day09:jquery get

---
+++ day09/06.jquery的ajax/sever.js
@@ -0,0 +1,43 @@
+const express = require("express");
+//注册一个服务
+const app = express();
+
+app.use(express.static("./public"));
+
+//获取post请求体 挂载到req上  只能处理urlencoded编码格式的请求
+app.use(express.urlencoded({
+    extended: true
+}))
+
+//获取post请求体，并挂载到req上  只能处理json字符串格式的请求
+app.use(express.json());
+
+app.get("/userinfo", (req, res) => {
+    //得到请求体
+    console.log(req.query);
+
+    //响应一个json数据
+    res.json({
+        name: "xiaowang",
+        age: 21
+    })
+})
+app.post("/userinfo1", (req, res) => {
+    //得到post请求的请求体
+    console.log(req.body);
+
+    //响应一个json数据
+    res.json({
+        name: "xiaozhang",
+        age: 12
+    })
+})
+
+//启动服务
+app.listen(3000, (err) => {
+    if (err) {
+        console.log("服务器启动错误" + err);
+        return;
+    }
+    console.log("服务器启动成功 http://127.0.0.1:3000");
+})
